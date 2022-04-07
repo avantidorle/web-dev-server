7c1724515e57e0564f67864b6601e011f1e0dc11
Avanti Dorle <avantidorle@Avantis-MacBook-Air.local>
2022-04-06 21:42:15 -0400

    first commit

---
+++ controllers/tuiter/tuits.js
@@ -0,0 +1,36 @@
+export default [
+  {
+          "_id": "123",
+          "userName": "Elon Musk",
+          "handle": "elonmusk",
+          "liked": true,
+          "time": "23h",
+          "avatarIcon": "/images/elonavatar.jpeg",
+          "title": "Amazing show about the @inspiration4x mission!",
+          "image": "/images/elonmusk.jpeg",
+          "contentTitle": "Most news outlets attempt to answer the question: What are the worst things happening on Earth today?",
+          "content": "It’s a big planet! Obviously, some bad things are happening somewhere at any given time, but focusing relentlessly on those does not give an accurate picture of reality.",
+          "comments": 101,
+          "likes": 345,
+          "dislikes": 0,
+          "retuits": 100,
+          "reshares": 500
+      },
+      {
+          "_id": "213",
+          "userName": "New York Post",
+          "handle": "nypost",
+          "time": "24h",
+          "liked": false,
+          "avatarIcon": "/images/nypostavatar.png",
+          "title": "Can`t sleep? Try these foods for a good night snooze https://trib.al/ZwVvTN4",
+          "image": "/images/nycpost.jpeg",
+          "contentTitle": "",
+          "content": "",
+          "comments": 2312,
+          "likes": 2223,
+          "dislikes": 0,
+          "retuits": 231,
+          "reshares": 12
+      }
+];
