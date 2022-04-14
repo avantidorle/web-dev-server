36e798fa358f16c78534f2c024298b154885c8c6
Avanti Dorle <avantidorle@Avantis-MacBook-Air.local>
2022-04-14 13:58:38 -0400

    Added connection string

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ import userController from './controllers/user-controller.js';
 import tuitsController from './controllers/tuits-controller.js';
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://newuser:newpassword@cluster0.mp43f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
